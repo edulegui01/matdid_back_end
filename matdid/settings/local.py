9ecdee56a8e1d5a41586cc766ef6c7ff1dcc6244
eduardo <edulegui@gmail.com>
2021-09-08 22:54:34 -0400

    se modifico las configuraciones para produccion

--- matdid/settings/local.py
+++ matdid/settings/local.py
@@ -1,5 +1,10 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
--- matdid/settings/production.py
+++ matdid/settings/production.py
@@ -1,9 +1,18 @@
 from .base import *
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = ["matdid-back-end.herokuapp.com"]
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'matdid',
+        'USER': 'edu',
+        'PASSWORD': 'lamasita123',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
     }
 }
 
