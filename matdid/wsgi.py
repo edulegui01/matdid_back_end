5c20ecc44461bf983f6777ad0eddfb138f99373c
eduardo <edulegui@gmail.com>
2022-04-07 20:36:50 -0400

    correccion de errores

--- matdid/wsgi.py
+++ matdid/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matdid.settings.produccion')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'matdid.settings.production')
 
 application = get_wsgi_application()
